f29b7aa3fa75f8af2ea068ddba67b46e8ec60ba5
asadullahqmr <asadqb16@gmail.com>
2020-06-16 22:36:03 +0800

    Finalised modal and the whole app in general.

--- app/src/main/java/com/example/booklistingapp/adapters/RecyclerAdapter.java
+++ app/src/main/java/com/example/booklistingapp/adapters/RecyclerAdapter.java
@@ -2,16 +2,23 @@ package com.example.booklistingapp.adapters;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Context;
 import androidx.annotation.NonNull;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.booklistingapp.R;
+import com.example.booklistingapp.models.BookInfo;
 import com.example.booklistingapp.models.Rack;
 
 import java.io.InputStream;
@@ -34,21 +41,49 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false);
         final ViewHolder vh = new ViewHolder(view);
-        final int currentInt = i;
+
+        //Set book dialog
+        final Dialog bookDialog = new Dialog(mContext);
+        bookDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        bookDialog.setContentView(R.layout.book_dialog);
+        final ImageView cover = (ImageView) bookDialog.findViewById(R.id.modalCover);
+        final TextView title = (TextView) bookDialog.findViewById(R.id.modalTitle);
+
+        //Set image views
         mImageView1 = view.findViewById(R.id.bookCover1);
         mImageView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int index = mAllRacks.get(vh.getAdapterPosition()).getBookOne().getIndex();
-                //Popup dialog for book.
+                BookInfo selectedBook = mAllRacks.get(vh.getAdapterPosition()).getBookOne();
+                try {
+                    InputStream ims = mContext.getAssets().open(selectedBook.getCover());
+                    Drawable d = Drawable.createFromStream(ims, null);
+                    cover.setImageDrawable(d);
+                }
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+                }
+                title.setText(selectedBook.getTitle());
+                bookDialog.show();
             }
         });
         mImageView2 = view.findViewById(R.id.bookCover2);
         mImageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int index = mAllRacks.get(vh.getAdapterPosition()).getBookTwo().getIndex();
-                //Popup dialog for book.
+                BookInfo selectedBook = mAllRacks.get(vh.getAdapterPosition()).getBookTwo();
+                try {
+                    InputStream ims = mContext.getAssets().open(selectedBook.getCover());
+                    Drawable d = Drawable.createFromStream(ims, null);
+                    cover.setImageDrawable(d);
+                }
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+                }
+                title.setText(selectedBook.getTitle());
+                bookDialog.show();
             }
         });
         return vh;
@@ -59,16 +94,18 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
         try {
             // Set the images
-            if(mAllRacks.get(i).getBookOne() != null) {
+            if(mAllRacks.get(i).getBookOne().getCover() != null) {
                 InputStream ims1 = mContext.getAssets().open(mAllRacks.get(i).getBookOne().getCover());
                 Drawable d1 = Drawable.createFromStream(ims1, null);
                 ((ViewHolder) viewHolder).mImage1.setImageDrawable(d1);
             }
-            if(mAllRacks.get(i).getBookTwo() != null) {
+            if(mAllRacks.get(i).getBookTwo().getCover() != null) {
                 InputStream ims2 = mContext.getAssets().open(mAllRacks.get(i).getBookTwo().getCover());
                 Drawable d2 = Drawable.createFromStream(ims2, null);
                 ((ViewHolder) viewHolder).mImage2.setImageDrawable(d2);
             }
+            else
+                ((ViewHolder) viewHolder).mImage2.setVisibility(View.INVISIBLE);
         }
         catch(Exception ex)
         {
